521bd803b01c745a61265b28731b8d303b0a227d
Евгений Афанасьев <evgenij29082002@gmail.com>
2023-05-27 00:47:24 +0800

    T in out reified

---
+++ src/main/kotlin/Variance.kt
@@ -0,0 +1,17 @@
+//class Barrel<out T>(val item: T)
+//
+//fun main(args: Array<String>) {
+//    var fedoraBarrel: Barrel<Fedora> =
+//        Barrel(Fedora("a generic-looking fedora", 15))
+//    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+//    lootBarrel = fedoraBarrel
+//    val myFedora: Fedora = lootBarrel.item
+//}
+class Barrel<in T>(item: T)
+
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> =
+        Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+    fedoraBarrel = lootBarrel
+}
